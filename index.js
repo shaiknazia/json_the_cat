9d2646c7c3e9f436c7d5b916ebcc99f4fa71f32c
Nazia Shaik <42496130+shaiknazia@users.noreply.github.com>
2020-03-13 01:42:36 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+const { fetchBreedDescription } = require('./breedFetcher');
+
+const breedName = process.argv[2];
+
+fetchBreedDescription(breedName, (error, desc) => {
+  if (error) {
+    console.log('Error fetch details:', error);
+  } else {
+    console.log(desc);
+  }
+});
+
+const { fetchBreedDescription } = require('./breedFetcher');
+
+const breedName = process.argv[2];
+
+fetchBreedDescription(breedName, (error, desc) => {
+  if (error) {
+    console.log('Error fetch details: ', error);
+  } else {
+    console.log(desc);
+  }
+});
